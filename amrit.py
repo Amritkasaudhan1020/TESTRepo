ce5bcc5ec32966f131e4b7f067db4a16875e79fa
Amrit Kasaudhan <90586202+Amritkasaudhan1020@users.noreply.github.com>
2022-11-30 11:48:04 +0530

    Add files via upload

---
+++ amrit.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+print("amrit kasaudhan got 50lpa offer")
+print(34+23)
+print(2 ** 4)
+print(10* 'amrit')
+print('C:\docs\namrit');
